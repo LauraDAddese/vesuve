94616482d7ccfa1fc56b65d604b8985bc5e56de3
Marine Hosch <marine.hosch@gmail.com>
2023-04-03 16:47:03 +0200

    debut carte dans js avec leaflet

---
+++ codeProjetVesuve/src/index.js
@@ -0,0 +1,9 @@
+let map = L.map('map').setView([46.7809, 6.6376], 14);
+
+let Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'jpg'
+});
