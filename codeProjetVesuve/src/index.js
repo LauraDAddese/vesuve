cef79b526232c8c13732b9a328eb1e09b271025d
Marine Hosch <marine.hosch@gmail.com>
2023-04-20 08:52:17 +0200

    essai carte leaflet, marche pas

--- codeProjetVesuve/src/index.js
+++ codeProjetVesuve/src/index.js
@@ -1,9 +1,11 @@
 let map = L.map('map').setView([46.7809, 6.6376], 14);
 
-let Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+let Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 1,
 	maxZoom: 16,
-	ext: 'jpg'
-});
+}).addTo(map);
+
+L.marker([46.7809, 6.6376]).addTo(map); //pour mettre des marqueurs, des displays cliquables. 
+
