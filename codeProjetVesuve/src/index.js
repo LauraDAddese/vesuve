889c47903f43c8451ea713e3c7b353a9ae36609d
Marine Hosch <marine.hosch@gmail.com>
2023-04-21 09:04:26 +0200

    faut parfois savoir lacher, la lave est toujours rouge

--- codeProjetVesuve/src/index.js
+++ codeProjetVesuve/src/index.js
@@ -1,8 +1,11 @@
 //let donneesgeopgraphiques =  "data/donneesgeopgraphiques.geojson";
 //let data = [donneesgeopgraphiques];
 geoJson.features[0].properties.tenCity = "changed value";
+let parse = JSON.stringify(geoJson);
+console.log(parse);
 
 let map = L.map('map').setView([40.82145693478615, 14.425858810559106], 12.2);
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 L.tileLayer(
   "https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png",
@@ -14,26 +17,73 @@ L.tileLayer(
 ).addTo(map);
 
 //tout afficher :
-//L.geoJSON(geoJson).addTo(map)
+// L.geoJSON(geoJson).addTo(map)
 
-const tabCoulees = [];
+const eruption = {
+	id: "",
+	annee: "",
+	geometrie: [],
+};
+
+const tabEruptions = [];
+
+// for(let i=0; i<geoJson.lenght; i++){
+//    console.log(geoJson[i].features.properties.id);
+// }
 
 //on affiche une éruption (un id en particulier) :
 for (let i = 0; i < geoJson.features.length; i++) {
 	geoJson.features.forEach(element => {
 		if (element.properties.id == i) {
 			//tabCoulees[i] = element;
+			//créer un nouveau tableau dans le tableau avec les coulees
+			//tabCoulees.push(element);
 
-			tabCoulees.push(element);
+			console.log(element);
+
+			// 	tabCoulees.forEach(id => {
+			
+	
+			// 	// changer le style de randomColor
+			// 		let	style = {
+			// 			color: randomColor,
+			// 		}
+			// })
 		}
-	})
+	});
 }
 
-L.geoJSON(tabCoulees[19]).addTo(map);
+L.geoJSON(geoJson.features, {
+    style: function(element) {
+        switch (element.properties.id) {
+            case '1': return {color: "#ff0000"};
+			case '2': return { color: "#0000ff" };
+			case '3':   return {color: "#0000ff"};
+			case '4':   return {color: "#0000ff"};
+            case '5':   return {color: "#0000ff"};
+            case '6':   return {color: "#0000ff"};
+            case '7':   return {color: "#0000ff"};
+            case '8':   return {color: "#0000ff"};
+            case '9':   return {color: "#000000"};
+            case '10':   return {color: "#345678"};
+            case '11':   return {color: "#0000ff"};
+            case '12':   return {color: "#0000ff"};
+            case '13':   return {color: "#5400ff"};
+            case '14':   return {color: "#5000ff"};
+            case '15':   return {color: "#0000ff"};
+            case '16':   return {color: "#4000ff"};
+            case '17':   return {color: "#1000ff"};
+            case '18':   return {color: "#2000ff"};
+            case '19':   return {color: "#3000ff"};
+        }
+    }
+}).addTo(map);
 
-console.log(tabCoulees[19]);
+
+// L.geoJSON(tabCoulees[19]).addTo(map);
+
+// console.log(tabCoulees[19]);
 
 // if (features[0].properties.id = 2) {
 // 	L.geoJSON(geoJson
-
 
