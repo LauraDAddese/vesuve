d22d02cf594ad307883f9f8d952e2861eb75a44a
Laura DAddese <laura.daddese@heig-vd.ch>
2023-05-04 12:32:38 +0200

    on y est presque...

--- codeProjetVesuve/src/index.js
+++ codeProjetVesuve/src/index.js
@@ -4,9 +4,8 @@ import scrollama from "scrollama";
 //import { ScrollamaInstance } from "scrollama";
 
 const scrolly = d3.select("#scrolly");
-const container = scrolly.select("#scrolly__graphic");
-const graphic = container.select(".scrolly__graphic");
-const volcan = scrolly.select(".volcan");
+const graphic = scrolly.select(".scroll__graphic");
+// const volcan = scrolly.select(".volcan");
 const text = scrolly.select(".scroll__text");
 const step = scrolly.selectAll(".step");
 const box = d3.selectAll(".box");
@@ -16,73 +15,98 @@ let scroller = scrollama();
 
 // generic window resize listener event
 function handleResize() {
-  // 1. update height of step elements
-  const stepHeight = Math.floor(window.innerHeight * 0.75);
-  step.style("height", `${stepHeight}px`);
+  
+    // 1. update height of step elements
+    const stepH = Math.floor(window.innerHeight * 0.75);
+    step.style("height", stepH + "px");
 
-  // 2. update width/height of graphic element
-  const figureHeight = window.innerHeight * 0.5;
-  const figureMarginTop = (window.innerHeight - figureHeight) / 2;
+    const figureHeight = window.innerHeight / 2;
+    const figureMarginTop = (window.innerHeight - figureHeight) / 2;
 
-  volcan.style("height", `${figureHeight}px`);
-  volcan.style("top", `${figureMarginTop}px`);
+    graphic.style("height", figureHeight + "px")
+    .style("top", figureMarginTop + "px");
 
-  // 3. tell scrollama to update new element dimensions
-  let volcanMargin = 32;
-  let textWidth = text.node().offsetWidth;
-  let volcanWidth = volcan.node().offsetWidth - textWidth - volcanMargin;
-  // make the height of 1/2 viewport
-  let volcanHeight = Math.floor(window.innerHeight / 2);
+// 3. tell scrollama to update new element dimensions
+    scroller.resize();
+  
 
-  graphic
-    .style("width", `${volcanWidth}px`)
-    .style("height", `${volcanHeight}px`);
 
-  //4. tell scrollama to update new element dimensions
-  scroller.resize();
 }
 
 // scrollama event handlers
-function handleStepEnter(response) {
-  // response = { element, direction, index }
+// function handleStepEnter(response) {
+//   // response = { element, direction, index }
+//   let currentIndex = response.index;
+//   let currentDirection = response.direction;
 
-  // fade in current step
+//   // fade in current step
+//   step.classed("is-active", function (d, i) {
+//     return i === currentIndex;
+//   });
+
+//   const svg = d3.select("#mon-svg").append("svg");
+//       // .attr("width", 200)
+//       // .attr("height", 200);
+
+//   switch (currentIndex) {
+//     case 0: // first step
+//       // Création de l'élément SVG
+//       svg.select("#mon-svg").remove();
+
+//       svg.append("image").attr("xlink:href", "svg/volcan1.svg");
+
+//       break;
+//     case 1: // second step
+//     svg.select("#mon-svg2").remove();
+//     svg.append("image").attr("xlink:href", "svg/volcan2.svg");
+//       break;
+//   }
+function handleStepEnter(response) {
+  let currentIndex = response.index;
   step.classed("is-active", function (d, i) {
-    return i === response.index;
+    return i === currentIndex;
   });
+
+  // Remove existing SVG element
+  d3.select("#mon-svg").select("svg").remove();
+
+  const svg = d3.select("#mon-svg")
+
+  switch (currentIndex) {
+    case 0: // first step
+      svg.append("image").attr("xlink:href", "svg/volcan1.svg");
+      break;
+    case 1: // second step
+      d3.select("#mon-svg2").select("svg").remove();
+      svg.append("image").attr("xlink:href", "svg/volcan2.svg");
+      break;
+    case 2: 
+      d3.select("#mon-svg2").select("svg").remove();
+      svg.append("image").attr("xlink:href", "svg/volcan3.svg");
+      break;
+    case 3: // second step
+      d3.select("#mon-svg2").select("svg").remove();
+      svg.append("image").attr("xlink:href", "svg/volcan4.svg");
+      break;
+  }
 
   //update graphic based on step here
   let stepData = step.attr("data-step");
 }
 
-function handleContainerEnter(response) {
-  // response = { direction }
-  // sticky the graphic
-  graphic.classed("is-fixed", true);
-  graphic.classed("is-bottom", false);
-}
+// function handleStepExit(response) {
+//   // response = { element, direction, index }
+//   let currentIndex = response.index;
+//   let currentDirection = response.direction;
 
-function handleContainerExit(response) {
-  // response = { direction }
-  // un-sticky the graphic, and pin to top/bottom of container
-  graphic.classed("is-fixed", false);
-  graphic.classed("is-bottom", response.direction === "down");
+//     if (response.index === 1) { // remove SVG container and image from second step
+//       d3.select("#my-svg-2").remove();
+//     }
+//     else if (response.index === 2) { // remove SVG container and image from third step
+//       d3.select("#my-svg-3").remove();
+//   };
+// }
 
-  // add color to current step only
-  step.classed("is-active", function (d, i) {
-    return i === response.index;
-  });
-
-  // change color of boxes based on step
-  box.style("background-color", function (d, i) {
-    return i === response.index ? "#FFA500" : "#eee";
-  });
-
-  // add map to step 4
-  // if (response.index === 3) {
-  //   createMap();
-  // }
-}
 function init() {
   // 1. force a resize on load to ensure proper dimensions are sent to scrollama
   handleResize();
@@ -93,16 +117,14 @@ function init() {
   scroller
     .setup({
       container: "#scrolly", // our outermost scrollytelling element
-      volcan: ".scrolly__graphic", // the graphic
+      graphic: ".scroll__graphic", // the graphic
       text: ".scroll__text", // the step container
       step: ".scroll__text .step", // the step elements
       offset: 0.5,
-      debug: false, // this being true is what makes the lines show up
+      debug: true, // this being true is what makes the lines show up
     })
-    .onStepEnter(handleStepEnter)
-    .onContainerEnter(handleContainerEnter)
-    .onContainerEnter(handleContainerEnter)
-    .onStepExit(handleContainerExit);
+    .onStepEnter(handleStepEnter);
+  // .onStepExit(handleStepExit);
 
   // setup resize event
   window.addEventListener("resize", handleResize);
