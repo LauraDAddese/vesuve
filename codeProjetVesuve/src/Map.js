37cbf937074963a31705507e083d88e848d79bcc
Marine Hosch <marine.hosch@gmail.com>
2023-05-06 08:54:25 +0200

    ajout side by side + révision code map, carte ancienne encore dupliquée

--- codeProjetVesuve/src/Map.js
+++ codeProjetVesuve/src/Map.js
@@ -1,127 +1,171 @@
 import { json } from "d3-fetch";
-
+import * as L from "leaflet";
+import "leaflet-side-by-side";
 
 const createMap = () => {
-	
-	
-	json("data/donneesgeographiques.geojson").then((data) => {
-  //afficher la carte :
-  let map = L.map("map").setView([40.82145693478615, 14.425858810559106], 12.2);
-  L.tileLayer("https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png", {
-    maxZoom: 20,
-    attribution:
-      '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-  }).addTo(map);
+  json("data/donneesgeographiques.geojson").then((data) => {
+    //afficher la carte :
+    let map = L.map("map").setView(
+      [40.82145693478615, 14.425858810559106],
+      12.2
+    );
+    //j'ajoute un premier layer (carte actuelle)
+    let layer1 = L.tileLayer(
+      "https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png",
+      {
+        maxZoom: 20,
+        attribution:
+          '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+      }
+    ).addTo(map);
 
-//   L.geoJSON(data).addTo(map);
+    //ajouter la deuxième carte pour le curseur(ancienne carte)
+    let layer2 = L.tileLayer("/src/carte_coulees_vesuve_1631-1831.png", {
+      attribution: "",
+      maxZoom: 20,
+      style: "width: 100%; height: 100%;",
+      style: "resize: both: 100%;",
+    }).addTo(map);
 
-	
-	
-	function getColor(d) {
-		return d == 1 ? 'd12d38' : //1701 couleur ok
-			d == 2 ? '#1f78b4' : //1820 couleur ok
-			d == 3 ? '#0002ff' : //1712 couleur ok
-			d == 4 ? '#ff7f00' : //1812 couleur ok
-			d == 5 ? '#e4d97a' : //1813 couleur ok
-			d == 6 ? '#000000' ://existe pas...
-			d == 7 ? '#ff4fa1' : //1810 couleur ok
-			d == 8 ? '#9c264f' : //1717 couleur ok
-			d == 9 ? '#fb99a4' : //1631 couleur ok
-			d == 10 ? '#d8a66a' : //1734 couleur ok
-			d == 11 ? '#ad4832' : //1822 couleur ok
-			d == 12 ? '#88a758' ://1779	 couleur ok
-			d == 13 ? '#57360f' : //1771 couleur ok
-			d == 14 ? '#977f62' : //1694 couleur ok
-			d == 15 ? '#b65e00' : //1805 couleur ok
-			d == 16 ? '#ef9c83' : //1754 couleur ok
-			d == 17 ? '#91e9f7' : //1786 couleur ok
-			d == 18 ? '#430109' : //1806 couleur ok
-			d == 19 ? '#261838' : // s.d. couleur ok
-			'#ffffff';
-	}
+    //create a Side by Side layer
+    L.control.sideBySide(layer1, layer2).addTo(map);
 
-//afficher le style de base des features
-	function style(feature) {
-		return {
-			fillColor: getColor(feature.properties.id),
-			fillOpacity: 0.6,
-			stroke: false,
-		};
-	}
-	L.geoJSON(data, { style: style }).addTo(map);
+    //fixer le zoom
+    function fixerZoom(map, level) {
+      map.setMinZoom(level);
+      map.setMaxZoom(level);
+      +map.setZoom(level);
+    }
 
+    fixerZoom(map, 12.5);
 
+    function getColor(d) {
+      return d == 1
+        ? "#d12d38" //1701 couleur ok
+        : d == 2
+        ? "#1f78b4" //1820 couleur ok
+        : d == 3
+        ? "#0002ff" //1712 couleur ok
+        : d == 4
+        ? "#ff7f00" //1812 couleur ok
+        : d == 5
+        ? "#e4d97a" //1813 couleur ok
+        : d == 6
+        ? "#ffffff" //existe pas...
+        : d == 7
+        ? "#ff4fa1" //1810 couleur ok
+        : d == 8
+        ? "#9c264f" //1717 couleur ok
+        : d == 9
+        ? "#fb99a4" //1631 couleur ok
+        : d == 10
+        ? "#d8a66a" //1734 couleur ok
+        : d == 11
+        ? "#ad4832" //1822 couleur ok
+        : d == 12
+        ? "#88a758" //1779	 couleur ok
+        : d == 13
+        ? "#57360f" //1771 couleur ok
+        : d == 14
+        ? "#977f62" //1694 couleur ok
+        : d == 15
+        ? "#b65e00" //1805 couleur ok
+        : d == 16
+        ? "#ef9c83" //1754 couleur ok
+        : d == 17
+        ? "#91e9f7" //1786 couleur ok
+        : d == 18
+        ? "#430109" //1806 couleur ok
+        : d == 19
+        ? "#261838" // s.d. couleur ok
+        : "#ffffff";
+    }
 
-	//fonction lors d'action au hover pour  changement d'opacité (ou de couleur à voir)
-function highlightFeature(e) {
-    let layer = e.target;
+    //afficher le style de base des features
+    function style(feature) {
+      return {
+        fillColor: getColor(feature.properties.id),
+        fillOpacity: 0.4,
+        stroke: false,
+      };
+    }
+    L.geoJSON(data, { style: style }).addTo(map);
 
-    console.log(layer.feature.properties.id)
+    //ajout de légende
+    let info = L.control();
+    info.onAdd = function (map) {
+      this._div = L.DomUtil.create("div", "info");
+      this.update(map);
+      return this._div;
+    };
 
-	//chercher le query>SelectAll pour les target ayant le meme id que la feature survolée
-	//console.log(e.target)
+    info.update = function (props) {
+      this._div.innerHTML =
+        "<h4>Année de l'éruption</h4>" +
+        (props ? "<b>" + props.annee + "</b><br />" : "Survolez une zone");
+    };
 
-    //Je sais pas comment récuper avec le querySelectorAll parce que les paths n'ont pas d'id
-    const sameID = document.querySelectorAll(`.leaflet-interactive`);
-    console.log(sameID);
+    info.addTo(map);
 
-    layer.setStyle({
-		fillOpacity: 1,
-		stroke: true,
-		color: "white",
-		opacity: 0.5,
-		weight: 2,
+    //fonction lors d'action au hover pour  changement d'opacité (ou de couleur à voir)
+    function highlightFeature(e) {
+      const layer = e.target;
+      const color = layer._path.getAttribute("fill");
+      const tabPaths = document.querySelectorAll(".leaflet-interactive");
 
-    });
+      tabPaths.forEach((el) => {
+        const fill = el.getAttribute("fill");
+        if (fill === color) {
+          el.style.fillOpacity = 1;
+          el.style.stroke = "white";
+          el.style.opacity = 1;
+          el.style.weight = 2;
+          el.parentNode.appendChild(el);
+        }
+      });
 
-	//tooltip : layer.bindTooltip(feature.properties.annee),
-	//.legend 
+      layer.bringToFront();
+      info.update(layer.feature.properties);
+    }
 
-    layer.bringToFront();
-}
+    //fonction pour remettre la couleur d'origine
+    function resetHighlight(e) {
+      const color = e.target._path.getAttribute("fill");
+      const tabPaths = document.querySelectorAll(".leaflet-interactive");
+      tabPaths.forEach((el) => {
+        if (el.getAttribute("fill") === color) {
+          el.style.fillOpacity = 0.4;
+          el.style.stroke = "";
+        }
+      });
+      info.update();
+    }
 
-//fonction pour remettre la couleur d'origine
-function resetHighlight(e) {
-    data.resetStyle(e.target);
-	}
-	
+    //fonction pour zoomer sur la zone au clic -- marche pas parce que déjà le popup...
+    // function zoomToFeature(e) {
+    //     map.fitBounds(e.target.getBounds());
+    // }
 
-//fonction pour zoomer sur la zone au clic -- marche pas parce que déjà le popup...
-// function zoomToFeature(e) {
-//     map.fitBounds(e.target.getBounds());
-// }
+    //fonction pour ajouter les listeners à chaque feature
+    data = L.geoJson(data, {
+      style: style,
+      onEachFeature: onEachFeature,
+    }).addTo(map);
 
-//fonction pour ajouter les listeners à chaque feature
-data = L.geoJson(data, {
-	style: style,
-	onEachFeature: onEachFeature
-}).addTo(map);
+    //fonction de listener pour les actions au hover et au mouseout (ou click)
+    function onEachFeature(feature, layer) {
+      layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+      });
+    }
 
-//fonction de listener pour les actions au hover et au mouseout (ou click)
-	
-	//////////////comment faire sur les ID et non pas sur les features ?? ////////////////////////////////////////////
+    data = L.geoJson(data, {
+      style: style,
+      onEachFeature: onEachFeature,
+    }).addTo(map);
+  });
+};
 
-function onEachFeature(feature, layer) {
-    layer.on({
-        //mouseover : layer.openPopup(feature.properties.annee),
-		mouseover : highlightFeature, 
-        mouseout : resetHighlight,
-		
-		//click : layer.bindPopup(feature.properties.annee)
-
-    });
-}
-
-data = L.geoJson(data, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(map);
-	
-	
-	//ajout d'un popup sur chaque feature indiquant la date --> la fonction pète le reste des actions,
-	//je l'ai mise direct dans la fonction onEachFeature plus haut pour l'instant.
-
-});
-}
-
-export default  createMap ;
+//export { createMap };
+export default createMap;
